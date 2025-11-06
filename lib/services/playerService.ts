53267e65c8f020bd3aac5edf2e4969ed203320de
k-mysa6505 <kota.mysa6505@gmail.com>
2025-11-06 17:58:44 +0900

    feat: プレイヤー管理機能実装

--- lib/services/playerService.ts
+++ lib/services/playerService.ts
@@ -4,10 +4,8 @@
 import {
   collection,
   doc,
-  setDoc,
   addDoc,
   updateDoc,
-  query,
   getDocs,
   onSnapshot,
   Timestamp
@@ -62,43 +60,46 @@ export async function getPlayers(roomId: string): Promise<Player[]> {
 
 /**
  * プレイヤー一覧をリアルタイム監視
- * TODO: 実装してください
- *
- * ヒント:
- * onSnapshotを使用
  */
 export function subscribeToPlayers(
   roomId: string,
   callback: (players: Player[]) => void
 ): () => void {
-  // TODO: 実装
-  throw new Error('Not implemented');
+  const playersRef = collection(db, 'rooms', roomId, 'players');
+  const unsubscribe = onSnapshot(playersRef, snapshot => {
+    const players: Player[] = snapshot.docs.map(doc => ({
+      playerId: doc.id,
+      ...(doc.data() as Omit<Player, 'playerId'>)
+    }));
+    callback(players);
+  });
+  return () => {
+    unsubscribe();
+  };
 }
 
 /**
  * プレイヤーの接続状態を更新
- * TODO: 実装してください
  */
 export async function updatePlayerOnlineStatus(
   roomId: string,
   playerId: string,
   isOnline: boolean
 ): Promise<void> {
-  // TODO: 実装
-  throw new Error('Not implemented');
+  const playerRef = doc(db, 'rooms', roomId, 'players', playerId);
+  await updateDoc(playerRef, { isOnline });
 }
 
 /**
  * プレイヤーのスコアを更新
- * TODO: 実装してください
  */
 export async function updatePlayerScore(
   roomId: string,
   playerId: string,
   score: number
 ): Promise<void> {
-  // TODO: 実装
-  throw new Error('Not implemented');
+  const playerRef = doc(db, 'rooms', roomId, 'players', playerId);
+  await updateDoc(playerRef, { score });
 }
 
 /**
